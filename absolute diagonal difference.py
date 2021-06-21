abc63f18d10aa21edb9fd3df31a2a62a73d4f7a2
ramya-muniraj <86179126+ramya-muniraj@users.noreply.github.com>
2021-06-21 12:39:13 +0530

    Update absolute diagonal difference.py

--- absolute diagonal difference.py
+++ absolute diagonal difference.py
@@ -1,9 +1,9 @@
 n=3
-suma=0
-sumb=0
+left_sum=0
+right_sum=0
 lst=[[7,9,8],[7,5,3],[9,-1,4]]
 for i in range(n):
-    suma+=lst[i][i]
+    left_sum+=lst[i][i]
 for i in range(n):
-    sumb+=lst[i][n-1-i]
-print(sumb-suma)
+    righ_sum+=lst[i][n-1-i]
+print(right_sum-left_sum)
